6530edc92ab5dc42aa7c97322edb5a7233b8a7d9
Anson Wen <AnsonWen888@windowslive.com>
2020-07-12 17:48:26 +1000

    Add project files.

---
+++ DesignCrowdTechChallenge/BusinessDayCounter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignCrowdTechChallenge
+{
+    class BusinessDayCounter
+    {
+        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            if (secondDate <= firstDate)
+                return 0;
+            int numberOfWeekDays = 0;
+            while (firstDate < secondDate)
+            {
+                firstDate = firstDate.AddDays(1);
+                if (firstDate.DayOfWeek != DayOfWeek.Saturday && firstDate.DayOfWeek != DayOfWeek.Sunday && firstDate != secondDate)
+                    numberOfWeekDays++;
+            }
+            return numberOfWeekDays;
+        }
+
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
+        {
+            if (secondDate <= firstDate)
+                return 0;
+            int numberOfWeekDays = 0;
+            while (firstDate < secondDate)
+            {
+                firstDate = firstDate.AddDays(1);
+                if (firstDate.DayOfWeek != DayOfWeek.Saturday && firstDate.DayOfWeek != DayOfWeek.Sunday && firstDate != secondDate && !publicHolidays.Contains(firstDate))
+                    numberOfWeekDays++;
+            }
+            return numberOfWeekDays;
+        }
+
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<PublicHolidayRule> publicHolidayRules)
+        {
+            if (secondDate <= firstDate)
+                return 0;
+            int numberOfWeekDays = 0;
+            List<DateTime> publicHolidays = new List<DateTime>();
+            foreach (PublicHolidayRule publicHolidayRule in publicHolidayRules)
+            {
+                if (firstDate.Year == secondDate.Year)
+                    publicHolidays.Add(publicHolidayRule.GetDateTime(firstDate.Year));
+                else { 
+                    publicHolidays.Add(publicHolidayRule.GetDateTime(firstDate.Year));
+                    publicHolidays.Add(publicHolidayRule.GetDateTime(secondDate.Year));
+                }
+            }
+            while (firstDate < secondDate)
+            {
+                firstDate = firstDate.AddDays(1);
+                if (firstDate.DayOfWeek != DayOfWeek.Saturday && firstDate.DayOfWeek != DayOfWeek.Sunday && firstDate != secondDate && !publicHolidays.Contains(firstDate))
+                    numberOfWeekDays++;
+            }
+            return numberOfWeekDays;
+        }
+
+    }
+}
---
+++ DesignCrowdTechChallenge/PublicHolidayRule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignCrowdTechChallenge
+{
+    class PublicHolidayRule
+    {
+
+        public PublicHolidayRule(int month, int date, bool carryOverDay)
+        {
+            IsFixed = true;
+            Month = month;
+            Day = date;
+            CarryOverDay = carryOverDay;
+        }
+        public PublicHolidayRule(int month, DayOfWeek dayOfWeek, int occurence, bool carryOverDay) // for a date rule based on occurence in month, e.g. 4th Thursday of November 
+                                                                                                   //not sure how to handle 'last' rule, maybe anything over 5 since the maximum occurence for any one day in a month is 5 
+        {
+            IsFixed = false;
+            CarryOverDay = carryOverDay;
+            Month = month;
+            DayOfWeekRule = dayOfWeek;
+            Occurence = occurence;
+        }
+
+        public DateTime GetDateTime(int year)
+        {
+            if (IsFixed)
+                return new DateTime(year, Month, Day);
+            else
+            {
+                DateTime date = new DateTime(year, Month, 01);
+                int startDayOfWeek = (int)date.DayOfWeek;
+                date = date.AddDays((int)DayOfWeekRule - startDayOfWeek); //gets first occurence of day of week
+                if (Occurence == 1)//returns first occurrence 
+                    return date;
+                else
+                {
+                    return date.AddDays(7 * (Occurence - 1));//places a great deal of trust in user, could be remedied by doing a for loop
+                }
+            }
+        }
+
+        public bool CarryOverDay { get; set; }
+        private bool IsFixed { get; set; }
+        private int Month { get; set; }
+        private int Day { get; set; }
+        private DayOfWeek DayOfWeekRule { get; set; }
+        private int Occurence { get; set; }
+
+
+    }
+}
